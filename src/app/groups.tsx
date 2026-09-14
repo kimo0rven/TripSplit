1d3fc1dd0f1b4ace8d7ab9a1107279ee7eda2227
Twigs <kimorvenvalencia@gmail.com>
2026-09-14 13:49:33 +0800

    added new pages

---
+++ src/app/groups.tsx
@@ -0,0 +1,41 @@
+import NavBar from '@/components/Navbar/Nav';
+import PageHeader from '@/components/ui/page-header';
+import { BrandColors, Spacing } from '@/constants/theme';
+import { ScrollView, StyleSheet, View } from 'react-native';
+
+export default function GroupsScreen() {
+
+    return (
+        <>
+            <ScrollView contentContainerStyle={styles.scrollContent} showsVerticalScrollIndicator={false}>
+                <View style={styles.container}>
+                    <View>
+                        <View>
+                            <PageHeader title="Group" />
+                        </View>
+                    </View>
+
+                    
+                </View>
+            </ScrollView>
+            <NavBar />
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    scrollContent: {
+        height: 100,
+        paddingBottom: 100,
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        backgroundColor: BrandColors.background,
+        paddingTop: 40,
+        paddingHorizontal: 25,
+        gap: Spacing.five,
+    },
+    
+
+});
